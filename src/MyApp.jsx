e0e9468ca32cf026ef2f6ee97142b018d805812e
zbartkus <zbartkuss3150@gmail.com>
2019-10-12 14:06:02 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React zbartkus3150</h1>  
+  <div>
+    <h1>Minimal React zbartkus3150</h1>  
+    <p>Bundle size: 59 bytes, Load time of the bundle: 65 ms, Last commit SHA1: 760da5c522369fda2052ce856de4b1201a5cf7d4</p>
+  </div>
 )
 
 export default MyApp
