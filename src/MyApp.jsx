4a9eb56095613a8cf1a1534715b0948feb838bb1
zbartkus <zbartkuss3150@gmail.com>
2019-10-14 13:13:47 +0200

    Lab1Ex3

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const generateArray = (n) => (Array.from({length: n}, (a, i) => i+1))
 
-const generateRandomArray = (n) => (Array.from({length: n}, (a, i) => i+1))
+const generateRandomArray = (n) => (Array.from({length: n}, () => Math.floor(Math.random() * 25) + 1))
 
 
 const MyApp = () => (
@@ -11,8 +11,12 @@ const MyApp = () => (
     <p>Bundle size: 59 bytes, Load time of the bundle: 65 ms, Last commit SHA1: 760da5c522369fda2052ce856de4b1201a5cf7d4</p>
   
     <ul>
-      <p>Array of elements form 1 to n (i.e. n=15)</p>
+      <p>Array of elements from 1 to n (i.e. n=15)</p>
       {generateArray(15).map(element => <li>{element}</li>)}
+    </ul>
+    <ul>
+      <p>Array of n random elements from 1 to 25 (i.e. n=10) </p>
+      {generateRandomArray(10).map(element => <li>{element}</li>)}
     </ul>
   </div> 
 )
