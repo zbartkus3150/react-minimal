fbac0504703d811f2bb475ea7c66785d9da592cc
zbartkus <zbartkuss3150@gmail.com>
2019-10-14 13:06:20 +0200

    Lab1Ex2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+const generateArray = (n) => (Array.from({length: n}, (a, i) => i+1))
+
 const MyApp = () => (
   <div>
     <h1>Minimal React zbartkus3150</h1>  
     <p>Bundle size: 59 bytes, Load time of the bundle: 65 ms, Last commit SHA1: 760da5c522369fda2052ce856de4b1201a5cf7d4</p>
-  </div>
+  
+    <ul>
+      <p>Array of elements form 1 to n (i.e. n=30)</p>
+      {generateArray(30).map(element => <li>{element}</li>)}
+    </ul>
+  </div> 
 )
 
 export default MyApp
