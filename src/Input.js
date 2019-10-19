050f45389a0b51d127f6f9109ccb592465cdb8d4
zbartkus <zbartkuss3150@gmail.com>
2019-10-19 14:02:43 +0200

    Lab2Ex2

--- src/Input.js
+++ src/Input.js
@@ -6,19 +6,35 @@ class Input extends React.Component {
         super(props);
         this.state={
             a:0,
-            b:0
+            b:0,
+            arr:[]
         }
         this.aValueChangedHandler=this.aValueChangedHandler.bind(this)
         this.bValueChangedHandler=this.bValueChangedHandler.bind(this)
+        this.generateArray=this.generateArray.bind(this)
     }
 
     aValueChangedHandler(event){
         console.log("Value changed a:"+event.target.value)
         this.setState({a:event.target.value});
+        this.generateArray(event.target.value, this.state.b)
     }
     bValueChangedHandler(event){
         console.log("Value changed b:"+event.target.value)
         this.setState({b:event.target.value});
+        this.generateArray(this.state.a, event.target.value)
+    }
+
+
+    generateArray(a, b){
+        a=parseInt(a)
+        b=parseInt(b)
+        this.state.arr=[]
+        if(a < b && a > 0){
+            var i
+            for(i = 0; i < b - a - 1; i++)
+                this.state.arr[i] = a + i + 1;
+        }
     }
 
 
@@ -31,6 +47,9 @@ class Input extends React.Component {
                 <br/>
                 <label>Number b: </label>
                 <input type="number" onChange={this.bValueChangedHandler} />
+                <ul>
+                    {this.state.arr.map(item => (<li key={item}>{item}</li>))}
+                </ul>
             </div>
         )
     
